7633b07bf3fa8ad344c8cf495146fa43f1f747a5
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2020-12-15 08:44:15 +0700

    tudien

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dictionary', function () {
+    return view('dictionary');
+});
+
+Route::post('/dictionary', function (Illuminate\Http\Request $request) {
+    $dictionary = [
+        "hello" => "xin chao",
+        "what" => "cái gì",
+        "book" => "sách",
+        "pen" => "bút"
+    ];
+    $searh = $request->input('search');
+    $flag = 0;
+    foreach ($dictionary as $key => $dictionarys){
+        if ($key == $searh){
+            echo "From: ".$key."<br>"."Meaning of words: ".$dictionarys;
+            echo "<br>";
+            $flag = 1;
+        }
+    }
+    if ($flag == 0){
+        echo "Did not find the words to look up.";
+    }
+    return view('dictionary');
+});
